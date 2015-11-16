7af613ed522c3f3e5294c41311a42b1922dce33e
Tushar Borole <tushariscoolster@gmail.com>
2015-11-16 19:03:53 +0530

    css chages

--- blurry.load.js
+++ blurry.load.js
@@ -6,13 +6,14 @@
             imageContainer = $(this)
         
         
-        parentContainer.addClass('blurry-load-container')
+        parentContainer.addClass('blurry-load-container');
+        imageContainer.addClass('img-blur')
 
         // 1: load small image and show it
         var img = new Image();
         img.src = imageContainer.attr('src');
         img.onload = function () {
-            imageContainer.addClass('img-blur loaded');
+            imageContainer.addClass('loaded');
         };
 
         // 2: load large image
